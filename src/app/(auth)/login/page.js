9c8801c18ac1cd4174dae0958986e260fc3c4c82
unknown <dejanmaric259@gmail.com>
2024-11-05 22:20:31 +0100

    general redesign on login page

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -3,11 +3,7 @@ import React from "react";
 import { LoginForm } from "@/app/components/LoginForm";
 
 const LoginPage = () => {
-  return (
-    <div className="flex h-screen justify-center items-center">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default LoginPage;
--- src/app/components/LoginForm/LoginForm.js
+++ src/app/components/LoginForm/LoginForm.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React, { useRef } from "react";
 import { Button } from "../ui";
 import { useAuthContext } from "@/app/contexts/AuthContext";
+import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
   const emailRef = useRef();
@@ -16,35 +17,28 @@ const LoginForm = () => {
   };
 
   return (
-    <form
-      onSubmit={(e) => handleSubmit(e)}
-      className="bg-gray-100 px-8 py-8 rounded-lg flex flex-col gap-8 w-fit h-fit"
-    >
-      <h2 className="text-3xl font-bold text-gray-950">Login</h2>
-      <div>
-        <p className="text-gray-950">Email</p>
-        <input
-          ref={emailRef}
-          className="bg-gray-700 rounded-lg px-4 py-2"
-          type="text"
-        />
-      </div>
-      <div>
-        <p className="text-gray-950">Password</p>
-        <input
-          ref={passwordRef}
-          className="bg-gray-700 rounded-lg px-4 py-2"
-          type="password"
-        />
-      </div>
-      <Button text="Login" />
-      <p className="text-gray-950">
-        Don't have an account ?{" "}
-        <Link className="text-green-500 " href="/register">
-          Sign Up
-        </Link>
-      </p>
-    </form>
+    <div className={styles.formWrapper}>
+      <form onSubmit={(e) => handleSubmit(e)} className={styles.formContainer}>
+        <h2 className={styles.title}>Login</h2>
+        <div className={styles.inputContainer}>
+          <p className={styles.label}>Email</p>
+          <input ref={emailRef} className={styles.input} type="text" />
+        </div>
+        <div className={styles.inputContainer}>
+          <p className={styles.label}>Password</p>
+          <input ref={passwordRef} className={styles.input} type="password" />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button text="Login" />
+        </div>
+        <p>
+          Don't have an account?{" "}
+          <Link href="/register" className={styles.linkText}>
+            Sign Up
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 };
 
